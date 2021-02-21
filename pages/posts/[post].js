b01462da11c2b376bae5be616de2845d69ff4c46
oreo2990 <knnt5323@gmail.com>
2021-02-21 14:36:56 +0900

    getServerSidePropsの引数検証([post].jsファイル)

--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -1,28 +1,34 @@
-export default function post({post}){
-    return (
-        <div>
-            <h1>POST（投稿）{post.id}</h1>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            
-            
-        </div>
-        );
-}
 
-// export async function getServerSideProps(context){
-//     // console.log(params);
-//     console.log(context);
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-//     const posts = await res.json();
-//     return {props:{posts}};
-// }
+// ①getServerSidePropsの引数がcontextの時、contextの中身確認する
+    export default function post({post}){
+        return (
+            <div>
+                <h1>POST（投稿）{post.id}</h1>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+            );
+    }
+    export async function getServerSideProps({params}){
+        // console.log(params);
+        console.log(params);
+        const id = params.post;
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const post = await res.json();
+        return {props:{post}};
+    }
 
-export async function getServerSideProps({params}){
-    // console.log(params);
-    console.log(params);
-    const id = params.post;
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const post = await res.json();
-    return {props:{post}};
-}
+
+
+// ②getServerSidePropsの引数がcontextの時、contextの中身確認する
+    // export default function post({post}){
+    //     return (<p>about</p>);
+    // }
+
+    // export async function getServerSideProps(context){
+    //     // console.log(params);
+    //     console.log(context);
+    //     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    //     const posts = await res.json();
+    //     return {props:{posts}};
+    // }
