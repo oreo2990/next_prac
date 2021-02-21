6717899e2ad73dc1ac5fe0d8b8f05eb96901d12f
oreo2990 <knnt5323@gmail.com>
2021-02-21 11:44:56 +0900

    3回めのコミット

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -8,5 +8,8 @@ export default function index({posts}){
 
 export async function getServerSideProps(){
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    
+    const posts = await res.json();
+    console.log(posts);
+    return {props: {posts}};
+
 }
